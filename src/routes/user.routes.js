f09bf90b7ea2119b731e3a7d2704bb19df72454d
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-15 17:51:19 +0530

    ☑️ More Routes added.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,23 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchedHistory,
   handleLogin,
   handleLogout,
+  refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyAccessToken } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
+//! unsecured routes
 userRouter.route("/register").post(
   upload.fields([
     {
@@ -24,8 +33,26 @@ userRouter.route("/register").post(
 );
 
 userRouter.route("/login").post(handleLogin);
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
 //! secured routes
 userRouter.route("/logout").get(verifyAccessToken, handleLogout);
+userRouter
+  .route("/change-password")
+  .post(verifyAccessToken, changeCurrentPassword);
+userRouter.route("/current-user").get(verifyAccessToken, getCurrentUser);
+userRouter.route("/c/:username").get(verifyAccessToken, getUserChannelProfile);
+userRouter
+  .route("/update-account")
+  .patch(verifyAccessToken, updateAccountDetails);
+
+userRouter
+  .route("/avatar")
+  .patch(verifyAccessToken, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("/cover-image")
+  .patch(verifyAccessToken, upload.single("coverImage"), updateUserCover);
+
+userRouter.route("/history").get(verifyAccessToken, getWatchedHistory);
 
 export { userRouter };
